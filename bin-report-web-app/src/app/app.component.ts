b56a18577915418d7da8a69af48f62eebf9918ca
angusho1 <angusho.77@gmail.com>
2022-01-15 18:10:00 -0800

    Set up Angular and Amplify

---
+++ bin-report-web-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'bin-report-web-app';
+}
